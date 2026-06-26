b14a590b590b24212049687d71481af63f086a27
Afsal s <cse.afxal@gmail.com>
2026-06-26 19:58:10 +0530

    express start

---
+++ 4.express/script.js
@@ -0,0 +1,9 @@
+// const logger = require("./utils");
+
+// console.log(logger());
+
+
+const {logger, myName } = require("./utils");
+
+console.log(logger());
+console.log(myName);
---
+++ 4.express/utils.js
@@ -0,0 +1,11 @@
+const logger = () =>{
+    return 2*5;
+};
+
+const myName = "Afsal";
+
+module.exports = {
+    logger,
+    myName,
+
+}
