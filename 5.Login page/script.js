24850f9655243823ef1fc021a08421bf34fa8c15
Afsal s <cse.afxal@gmail.com>
2026-06-29 22:13:05 +0530

    login page compeleted

--- 5.Login page/script.js
+++ 5.Login page/script.js
@@ -1,10 +1,23 @@
 const express = require("express");
-
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static("views"));
 
 
+const emailDB = "cse.afxal@gmail.com";
+const passwordDB = "1234";
 
+app.post("/login",(req,res)=> {
+    const {email, password} = req.body;
+
+    if (email === emailDB && password === passwordDB) {
+        res.send("Login Sucessful");
+    }else {
+        res.send("Login failed")
+    }
+});
 
 const PORT = process.env.PORT || 3001;
 
